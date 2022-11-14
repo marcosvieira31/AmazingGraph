3a0b6daa89a3302c1074e0851f07cc4254e5664e
Marcos Vieira <marcosnd.31@gmail.com>
2022-11-14 18:35:16 -0300

    Ajuste no container article e hover da imagem

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ function botao(){
     const newImage = document.createElement("img")
     newImage.src = `https://source.unsplash.com/random/800x600?=${Date.now()}`
     document.querySelector("#container").appendChild(newImage)
-    console.log(newImage.src)    
-
-    console.log(typeof(image))
+    console.log(newImage.src)
 }
 
 function menu() {

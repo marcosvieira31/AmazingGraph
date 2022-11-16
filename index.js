8e180e7105aaa386cfa8c6c6bdbe7e4a3a4ec4da
Marcos Vieira <marcosnd.31@gmail.com>
2022-11-16 20:15:25 -0300

    Zoom no hover, ajustes de layout e responsividade

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function botao(){
-    const image = document.querySelector("img")   
+    const image = document.getElementById("main-img")   
  
     image.remove()
     const newImage = document.createElement("img")
-    newImage.src = `https://source.unsplash.com/random/800x600?=${Date.now()}`
-    document.querySelector("#container").appendChild(newImage)
-    console.log(newImage.src)
+    newImage.setAttribute("id", "main-img")
+    newImage.setAttribute("src",`https://source.unsplash.com/random/800x600?=${Date.now()}`)
+    document.querySelector("#img-container").appendChild(newImage)
 }
 
 function menu() {
